135cc21315cb3457ab652619de7dabc8352f554b
vuyanzi <vuyanziglorious@gmail.com>
2019-04-25 14:40:20 +0300

    Test

---
+++ add/lib/add.ex
@@ -0,0 +1,18 @@
+defmodule Add do
+  @moduledoc """
+  Documentation for Add.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Add.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
